25caaeea3a05d36a6ddf0485b444dc9361a1a1b7
Miguel Mendoza <info@miguelmendoza.dj>
2017-03-20 19:29:48 +0100

    adds api

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,8 +1,8 @@
 module Api::V1
   class ProjectsController < ApiController
     def index
-      @projects = Project.fetch_all(filter_params)
-      render json: @projects, each_serializer: ProjectSerializer
+      projects = Project.fetch_all(filter_params)
+      render json: projects, each_serializer: ProjectSerializer
     end
     private
       def filter_params
