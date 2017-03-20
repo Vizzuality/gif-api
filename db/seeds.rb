f6517eee7276832af6cc9021a6af03376f2c8d14
Miguel Mendoza <info@miguelmendoza.dj>
2017-03-20 14:31:12 +0100

    adds seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+if CoBenefitsOfIntervention.all.size == 0
+  bf = ['shoreline stabilization & accretion', 'employment, enterprise & property', 'public health','tourism & recreation', 'biodiversity', 'carbon sequestration', 'other ecosystem services']
+  bf.each do |b|
+    CoBenefitsOfIntervention.create!(name: b)
+  end
+end
+if PrimaryBenefitsOfIntervention.all.size == 0
+  bf = ['coastal energy management', 'redirection & drainage of flood & storm', 'water storage & infiltration of flood & storm water', 'wind speed reduction', 'landslides, erosion & siltation', 'control shoreline stabilization & accretion']
+  bf.each do |b|
+    PrimaryBenefitsOfIntervention.create!(name: b)
+  end
+end
+if NatureBasedSolution.all.size == 0
+  nbs = ['urban green spaces', 'forests & vegetation', 'rangelands', 'rivers & floodplains', 'terrestrial wetlands', 'coastal wetlands', 'dunes', 'mangroves', 'coral reefs & living shorelines', 'other']
+  nbs.each do |n|
+    NatureBasedSolution.create!(name: n)
+  end
+end
+if HazardType.all.size == 0
+  ht = ['urban', 'river', 'coastal']
+  ht.each do |h|
+    HazardType.create!(name: h)
+  end
+end
