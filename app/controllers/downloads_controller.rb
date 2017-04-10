41554b9b6ac993a7fb0280aea81c788ca374409e
Miguel Mendoza <info@miguelmendoza.dj>
2017-04-10 11:09:12 +0200

    adds fake downloads

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,8 +1,8 @@
 class DownloadsController < ApplicationController
   def download
-    if params[:id] == 'project.pdf'
+    if params[:id] == 'project'
       send_file Rails.root.join('public/downloads', 'project.pdf'), type: "application/pdf", x_sendfile: true
-    elsif params[:id] == 'projects.csv'
+    elsif params[:id] == 'projects'
       send_file Rails.root.join('public/downloads', 'projects.csv'), type: "text/csv", x_sendfile: true
     end
   end
