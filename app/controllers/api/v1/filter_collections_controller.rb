69f85b4ac2370299d35c919a6c8ef90ec6af5e2a
Miguel Mendoza <info@miguelmendoza.dj>
2017-05-04 17:04:46 +0200

    fixes 401 on filters

--- app/controllers/api/v1/filter_collections_controller.rb
+++ app/controllers/api/v1/filter_collections_controller.rb
@@ -1,5 +1,6 @@
 module Api::V1
   class FilterCollectionsController < ApiController
+    skip_before_action :authenticate_request
     def index
       filters = FilterCollection.fetch_all
       render json: filters, root: true, status: 200
