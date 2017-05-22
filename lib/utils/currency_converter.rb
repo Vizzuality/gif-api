463f9aaf81a9ac9ebb7b6e4e8b1ab9872838b1ac
Miguel Mendoza <miguel.mendoza@vizzuality.com>
2017-05-22 20:21:55 +0200

    adds currency converter

--- lib/utils/currency_converter.rb
+++ lib/utils/currency_converter.rb
@@ -1,5 +1,22 @@
 module CurrencyConverter
-  def self.convert(amount, currency, date = nil)
-    response = Faraday.get "http://www.apilayer.net/api/live?access_key=#{ENV['CURRENCY_LAYER_KEY']}"
+  def self.convert(amount, currency, year = nil)
+    if year.is_a? Integer
+      year = 1999 if year < 1999
+      d = Date.new(year,1,1)
+    else
+      raise Exception.new('Invalid call')
+    end
+    d = d.strftime("%Y-%m-%d")
+    api_call = "http://www.apilayer.net/api/historical?access_key=#{ENV['CURRENCY_LAYER_KEY']}&currencies=#{currency}&date=#{d}"
+    response = Faraday.get api_call
+    if response.status == 200
+      json = JSON.parse(response.body)
+      change = json["quotes"]["USD#{currency}"]
+      result = amount/change
+      raise Exception.new(response.status) unless json["success"] == true
+    else
+      raise Exception.new(response.status)
+    end
+    result
   end
 end
