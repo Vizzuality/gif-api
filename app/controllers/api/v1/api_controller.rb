da4a7813c63e2d11230e1527ad1704c6a845c5ee
Miguel Mendoza <miguel.mendoza@vizzuality.com>
2017-05-25 12:42:56 +0200

    adds cors headers

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -3,6 +3,14 @@ module Api
     class ApiController < ActionController::Base
       rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
       before_action :authenticate_request
+      before_action :set_cors
+
+      def set_cors
+        headers['Access-Control-Allow-Origin'] = '*'
+        headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+        headers['Access-Control-Request-Method'] = '*'
+        headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+      end
 
       attr_reader :current_user
       helper_method :current_user
