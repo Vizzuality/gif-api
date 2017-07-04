548e6d0cdcbb1bf62a2301e442b8087828760a88
Miguel Mendoza <info@miguelmendoza.dj>
2017-07-04 16:36:49 +0200

    orders hazard types in filters api

--- lib/utils/filter_collection.rb
+++ lib/utils/filter_collection.rb
@@ -5,15 +5,23 @@ module FilterCollection
     countries = Location.joins(:projects).select(:iso, :adm0_name).order('adm0_name ASC').compact.uniq{ |u| u.iso }
     organizations = Organization.all.select(:id, :name).order('name ASC')
     donors = Donor.all.select(:id, :name).order('name ASC')
-    hazard_types = HazardType.all.select(:id, :name).order('name ASC')
+    hazard_types = HazardType.all.select(:id, :name).order("
+            CASE
+              WHEN name = 'coastal flooding' THEN '1'
+              WHEN name = 'river flooding' THEN '2'
+              WHEN name = 'urban flooding' THEN '3'
+              WHEN name = 'drought' THEN '4'
+              WHEN name = 'landslides' THEN '5'
+             END
+        ")
     intervention_types = Project::INTERVENTION_TYPES.sort_by{ |m| m.downcase }
     nature_based_solutions = NatureBasedSolution.all.select(:id, :name).order('name ASC')
     currencies = Currency.all.select(:id, :iso, :name).order('name ASC')
     primary_benefits = PrimaryBenefitsOfIntervention.all.select(:id, :name).order('name ASC')
     co_benefits = CoBenefitsOfIntervention.all.select(:id, :name).order('name ASC')
     implementation_statuses = Project::IMPLEMENTATION_STATUSES.sort_by{ |m| m.downcase }
-    cost_min = Project.where.not(costs_usd: nil).order('costs_usd ASC').limit(1).pluck(:costs_usd).join.to_f
-    cost_max = Project.where.not(costs_usd: nil).order('costs_usd DESC').limit(1).pluck(:costs_usd).join.to_f
+    cost_min = 0.15
+    cost_max = 5000.0
     filters = {}
     filters[:scales] = scales
     filters[:regions] = regions
