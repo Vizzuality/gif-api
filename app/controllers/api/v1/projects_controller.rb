ef0a12f09f34c6af51d348a846c7e92dd7e542c5
Tiago Santos <santos.tiago@gmail.com>
2018-02-13 12:07:48 +0000

    Changed the cached version of the projects to include the relationships

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -7,20 +7,19 @@ module Api::V1
         format.json do
           # rubocop:disable LineLength
           key_string = "projects#{request.url}_#{request.query_string}_#{Project.order('updated_at DESC').first.try(:updated_at)}_json".parameterize
-          projects = Rails.cache.fetch(key_string) { Project.fetch_all(filter_params).each {|x| ProjectSerializer.new(x)}.to_json }
+          projects = Rails.cache.fetch(key_string) { render json: Project.fetch_all(filter_params), each_serializer: ProjectSerializer }
           # rubocop:enable LineLength
-          render json: projects
+          render json: projects rescue projects
         end
         format.csv do
           # rubocop:disable LineLength
           key_string = "projects#{request.url}_#{request.query_string}_#{Project.order('updated_at DESC').first.try(:updated_at)}_csv".parameterize
-          projects = Rails.cache.fetch(key_string) { Project.fetch_all(filter_params).to_csv }
+          projects = Rails.cache.fetch(key_string) { Project.fetch_all(filter_params) }
           # rubocop:enable LineLength
-          send_data projects, filename: "users-#{Date.today}.csv"
+          send_data projects.to_csv, filename: "users-#{Date.today}.csv"
         end
       end
     end
-
     def show
       render json: @project, serializer: ProjectSerializer
     end
