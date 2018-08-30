f3b209f4df4a171535e2501c2bcf68d652ecf18a
David Inga <david.inga@vizzuality.com>
2018-08-30 11:46:54 +0200

    default sort for donos and organizations

--- app/models/donor.rb
+++ app/models/donor.rb
@@ -9,6 +9,7 @@
 #
 
 class Donor < ApplicationRecord
+  default_scope { order(name: :desc) }
   validates :name, presence: true, uniqueness: true
   has_many :donors_projects, dependent: :nullify
   has_many :projects, through: :donors_projects, dependent: :nullify
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -9,6 +9,7 @@
 #
 
 class Organization < ApplicationRecord
+  default_scope { order(name: :desc) }
   has_many :organizations_projects, dependent: :nullify
   has_many :projects, through: :organizations_projects, dependent: :nullify
   validates :name, presence: true, uniqueness: true
